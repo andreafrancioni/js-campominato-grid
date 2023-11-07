2224958e7404050761a2246d28f0d258958bdce3
“andreafrancioni” <andreafrancioni.work@gmail.com>
2023-11-08 00:14:47 +0100

    crea griglie in base alla difficoltà

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
-let numCells= 100;
+let gameMode = document.getElementById("gamemode");
+let numCells;
 let grid = document.getElementById("grid");
+let playBtn = document.getElementById("playBtn");
+let squareDim;
 
-for (let i = 1; i <= numCells; i++){
-    let oneCell = buildSquare(i);
-    grid.appendChild(oneCell);
-    oneCell.innerHTML = i;
-}
+/*** PULSANTE PLAY AVVIO GIOCO ***/
+playBtn.addEventListener("click", function() {
+    
+    /*** VERIFICA MODALITA DI GIOCO ***/
+    if(gameMode.value == 1){
+        numCells = 100;
+        squareDim = "squareEasy"
+    }else if(gameMode.value == 2){
+        numCells = 81;
+        squareDim = "squareMedium"
+    }else{
+        numCells = 49;
+        squareDim = "squareDifficult"
+    }
+
+
+
+    for (let i = 1; i <= numCells; i++){
+        let oneCell = buildSquare(i);
+        grid.appendChild(oneCell);
+        oneCell.innerHTML = i;
+    }
+});
+
+
+
+
 
 /*** FUNZIONE CREA QUADRATI***/
 function buildSquare(number) {
     const cell = document.createElement("div");
-    cell.classList.add("square");
+    cell.classList.add(squareDim);
 
     cell.addEventListener("click", function(){
         cell.classList.toggle("selected");
